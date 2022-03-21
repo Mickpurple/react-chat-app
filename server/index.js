792ab383956256556abb0df8c2bf3d46000ef82a
Mickpurple <themickpurple@gmail.com>
2022-03-21 14:09:24 +0530

    react chat app using socket.io version 1.0

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server, Socket } = require("socket.io");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("user connected " + socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected ", socket.id);
+    });
+    socket.on("joinRoom", (data) => {
+        socket.join(data);
+        console.log("user "+socket.id+" joined room "+data);
+    });
+    socket.on("sendMessage", (data) => {
+        socket.to(data.room).emit("receiveMessage", data);
+    })
+});
+
+server.listen(3001, function() {
+    console.log("server started on port 3001");
+});
